066ddc538f4f7048b01fb83df188a15886ccb0c1
Babak <jahanibabak80@gmail.com>
2023-03-14 17:21:31 +0330

    remove icon added

--- src/Components/SelectComment/ShowSelectedComponent.js
+++ src/Components/SelectComment/ShowSelectedComponent.js
@@ -1,10 +1,13 @@
 import React, { useContext } from "react";
 import { Context } from "../Context";
+//BsTrashFill
+import { BsTrashFill } from "react-icons/bs";
+import { IconContext } from "react-icons";
 
 const ShowSelectedComponent = () => {
   const { SelectComment } = useContext(Context);
   return (
-    <div className="w-5/6 min-h-[20vh] mt-10 text-lg mx-auto bg-[#2E4F4F] text-white rounded-xl px-4 py-2 shadow-md shadow-[#0E8388] flex flex-col justify-between">
+    <div className="w-5/6 min-h-[20vh] mt-10 text-lg mx-auto bg-[#2E4F4F] text-white rounded-xl px-4 py-2 shadow-md shadow-[#0E8388] flex flex-col justify-between relative">
       <div>
         <div className="flex">
           <p className="mr-2">Name:</p>
@@ -24,6 +27,18 @@ const ShowSelectedComponent = () => {
           <p className="mr-2">Body:</p>
           <p>{SelectComment.massage}</p>
         </div>
+      </div>
+      <div className="absolute top-5 right-5">
+        <IconContext.Provider
+          value={{
+            color: "",
+            className: "cursor-pointer hover:text-[#0E8388] text-[#CBE4DE]",
+          }}
+        >
+          <div>
+            <BsTrashFill />
+          </div>
+        </IconContext.Provider>
       </div>
     </div>
   );
