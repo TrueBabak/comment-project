82ae251f1f794a1e0d8f34ceb927642ab3e48b60
Babak <jahanibabak80@gmail.com>
2023-03-14 17:11:36 +0330

    design fixed

--- src/Components/AllComments/index.js
+++ src/Components/AllComments/index.js
@@ -10,7 +10,7 @@ const AllComments = () => {
     axios.get("employees").then((res) => setComment(res.data));
   }, []);
   return (
-    <div className="w-5/6 min-h-[20vh] bg-[#2E4F4F] text-white mx-auto relative rounded-xl px-4 py-6 shadow-md shadow-[#0E8388] grid grid-cols-4 gap-4">
+    <div className="w-5/6 min-h-[15vh] bg-[#2E4F4F] text-white mx-auto relative rounded-xl px-4 shadow-md shadow-[#0E8388] grid grid-cols-4 gap-4 items-center">
       {!Comment ? (
         <Loading color={"#CBE4DE"} />
       ) : (
