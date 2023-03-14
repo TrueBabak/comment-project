1238ccb307d7f84737674711e5212cd996968671
Babak <jahanibabak80@gmail.com>
2023-03-14 15:22:33 +0330

    select comment by id request on database

--- src/Components/Context/index.js
+++ src/Components/Context/index.js
@@ -1,8 +1,11 @@
+import axios from "axios";
 import { createContext, useState } from "react";
 
 export const ContextApi = {
   Comment: null,
   setComment: () => {},
+  SelectComment: null,
+  setSelectComment: () => {},
   getIdHandler: () => {},
 };
 export const Context = createContext(ContextApi);
@@ -10,11 +13,24 @@ export const Context = createContext(ContextApi);
 // Provider
 const ContextsProvider = ({ children }) => {
   const [Comment, setComment] = useState(null);
-  const getIdHandler = (userId) => {
+  const [SelectComment, setSelectComment] = useState(null);
+  const getIdHandler = async (userId) => {
     console.log(userId);
+    await axios.get(`employees/${userId}`).then((res) => {
+      setSelectComment(res.data);
+    });
+    console.log(SelectComment);
   };
   return (
-    <Context.Provider value={{ Comment, setComment, getIdHandler }}>
+    <Context.Provider
+      value={{
+        Comment,
+        setComment,
+        getIdHandler,
+        SelectComment,
+        setSelectComment,
+      }}
+    >
       {children}
     </Context.Provider>
   );
